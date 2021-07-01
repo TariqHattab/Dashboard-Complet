da051cef1277e8930227624629fab01553cd2a9b
TariqHattab <trqzh123@gmail.com>
2021-07-02 01:41:55 +0300

    commit 5

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:new_app/constants.dart';
 
 import 'components/side_menu.dart';
 
@@ -16,7 +17,28 @@ class MainScreen extends StatelessWidget {
             Expanded(
               flex: 5,
               child: Row(
-                children: [],
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16),
+                    child: Text(
+                      'Dashboard',
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                  ),
+                  Spacer(),
+                  Expanded(
+                    child: TextField(
+                      decoration: new InputDecoration(
+                        fillColor: secondaryColor,
+                        filled: true,
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
               ),
             ),
           ],
