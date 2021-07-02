e31eb134221a0ca3aaf9ff9e437610f00a3050b9
TariqHattab <trqzh123@gmail.com>
2021-07-03 00:18:23 +0300

    commit 7

--- lib/screens/dashboard_screen/dashboard_screen.dart
+++ lib/screens/dashboard_screen/dashboard_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:new_app/constants.dart';
+import 'package:new_app/models/MyFiles.dart';
 
 import 'components/header.dart';
 
@@ -11,13 +14,114 @@ class DashboardScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: SingleChildScrollView(
-        padding: const EdgeInsets.all(16),
+        padding: const EdgeInsets.all(defualtPadding),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
           children: [
             Header(),
+            SizedBox(
+              height: defualtPadding,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 5,
+                  child: Container(
+                    height: 700,
+                    color: Colors.red,
+                  ),
+                ),
+                SizedBox(
+                  width: defualtPadding,
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    height: 700,
+                    padding: const EdgeInsets.all(defualtPadding),
+                    decoration: BoxDecoration(
+                        color: secondaryColor,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Storage Details',
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                        Container(
+                          height: 200,
+                          width: 200,
+                          color: Colors.amber,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        ...demoMyFiles
+                            .map((e) => RecentFiles(
+                                  title: e.title,
+                                  subTitle: e.numOfFiles.toString(),
+                                  data: e.totalStorage,
+                                  svgUrl: e.svgSrc,
+                                ))
+                            .toList() ///////////// the error is here try to fix
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class RecentFiles extends StatelessWidget {
+  RecentFiles({
+    Key? key,
+    String? title,
+    String? subTitle,
+    String? svgUrl,
+    String? data,
+  }) : super(key: key);
+
+  late String title, subTitle, svgUrl, data;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: secondaryColor,
+          border: Border.all(color: Colors.white10),
+          borderRadius: BorderRadius.circular(10)),
+      padding: const EdgeInsets.symmetric(
+          horizontal: defualtPadding, vertical: defualtPadding / 2),
+      child: Row(
+        children: [
+          SvgPicture.asset(
+            svgUrl,
+            // color: secondaryColor,
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(title),
+              Text(
+                subTitle,
+                style: TextStyle(color: Colors.white54, fontSize: 12),
+              )
+            ],
+          ),
+          Spacer(),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(data),
+          )
+        ],
       ),
     );
   }
